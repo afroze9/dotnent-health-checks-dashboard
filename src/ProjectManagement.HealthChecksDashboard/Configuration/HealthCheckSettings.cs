8e04d9874870b157ce1ff333aa5ff76129c6e237
Afroze Amjad <afroze.amjad@confiz.com>
2023-05-04 13:54:06 +0500

    Added health-checks-dashboard to solution and scripts

---
+++ src/ProjectManagement.HealthChecksDashboard/Configuration/HealthCheckSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectManagement.HealthChecksDashboard.Configuration;
+
+public class HealthCheckSettings
+{
+    public HealthCheckClient[] Clients { get; set; } = Array.Empty<HealthCheckClient>();
+}
+
+public class HealthCheckClient
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+}
